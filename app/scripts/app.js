75b16349505b0900a730ac9078cdc119a58d71d4
ShadowBan <chris.kincanon@gmail.com>
2015-03-16 12:50:03 -0700

    Adding in layout tag

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,5 @@
 (function (document) {
   'use strict';
-
 // wrap document so it plays nice with other libraries
 // http://www.polymer-project.org/platform/shadow-dom.html#wrappers
 })(wrap(document));
